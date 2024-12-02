674cc7d4d70c2504ef5807cdf7b1b4fb9c0dee5d
Rinorragi <rinorragi@gmail.com>
2024-12-02 12:38:48 +0200

    Add day 2

---
+++ day02.fsx
@@ -0,0 +1,70 @@
+// Macro to get running time
+#time 
+
+// Newline
+let nl = "\n"
+
+let parseInput (filePath : string)  =
+    System.IO.File.ReadAllLines filePath
+    |> Array.map (fun f -> 
+        f.Split(" ", System.StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map int
+        |> List.ofArray)
+    |> List.ofArray
+
+let levelIncreasePairwiser (report: int list) = 
+    let levelPairs = 
+        report 
+        |> List.pairwise
+        |> List.groupBy (fun (first, second) -> if first < second then true else false)
+        |> List.map fst
+    
+    let trues = levelPairs |> List.where (fun f -> f = true)
+    let falses = levelPairs |> List.where (fun f -> f = false)
+    if trues > falses then report |> List.pairwise
+    else report |> List.rev |> List.pairwise
+
+let isPairSafe (first: int, second: int) = first < second && second - first <= 3
+
+let safetyInspection (report: int list) =
+    report
+    |> levelIncreasePairwiser
+    |> List.forall (isPairSafe)
+
+let safetyInspectionWithDampener (report: int list) =
+    let reportsRanked =
+        report
+        |> levelIncreasePairwiser
+        |> List.map (fun levelTuple -> (isPairSafe levelTuple, levelTuple))
+    let unsafeLevels = reportsRanked |> List.where (fst >> not)
+    if unsafeLevels.Length = 0 then true
+    else false
+
+let safetyInspectionBruteDamper (report: int list) =
+    if safetyInspectionWithDampener report then true
+    else 
+        let bruteReports = 
+            report |> List.mapi (fun i _ -> report |> List.removeAt i |> safetyInspectionWithDampener)
+        bruteReports |> List.exists (fun f -> f = true)
+
+let reportCalculator (reports: bool list) = reports |> List.where (fun f -> f = true) |> List.length
+
+parseInput "./input/day02_example.txt" 
+|> List.map safetyInspection
+|> reportCalculator
+|> printfn "Example answer1: %d"
+
+parseInput "./input/day02_example.txt" 
+|> List.map safetyInspectionBruteDamper
+|> reportCalculator
+|> printfn "Example answer2: %d"
+
+parseInput "./input/day02.txt" 
+|> List.map safetyInspection
+|> reportCalculator
+|> printfn "Answer1: %d"
+
+parseInput "./input/day02.txt" 
+|> List.map safetyInspectionBruteDamper
+|> reportCalculator
+|> printfn "Answer2: %d"
