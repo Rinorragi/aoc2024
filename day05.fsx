a78c525b9b48a86ba3684dd8bd58d73e39759ff6
Joona Immonen <14329249+Rinorragi@users.noreply.github.com>
2025-02-14 08:35:12 +0200

    Add day 5 part 2

--- day05.fsx
+++ day05.fsx
@@ -21,24 +21,22 @@ let parseInput (filePath : string)  =
         |> fun s -> s.Split(nl+nl, System.StringSplitOptions.RemoveEmptyEntries)
     (parseString inputs.[0] "|", parseString inputs.[1] ",")
 
+let filterRules (rules: int list list) (update: int list) =
+    rules 
+    |> List.map (fun rule -> 
+        let intersectCount = 
+            update 
+            |> Set.ofList 
+            |> Set.intersect (rule 
+            |> Set.ofList) |> Set.count 
+        match intersectCount with 
+        | 2 -> Some(rule)
+        | _ -> None)
+    |> List.filter Option.isSome
+    |> List.map Option.get
+
 let filterPrintableOrders (rules: int list list) (updates: int list list) =
-    let filterRules (update: int list) =
-        rules 
-        |> List.map (fun rule -> 
-            let intersectCount = 
-                update 
-                |> Set.ofList 
-                |> Set.intersect (rule 
-                |> Set.ofList) |> Set.count 
-            match intersectCount with 
-            | 2 -> Some(rule)
-            | _ -> None)
-        |> List.filter Option.isSome
-        |> List.map Option.get
-
-
     let rec pagePrintable (originalUpdate: int list) (update : int list) (updateRules: int list list) =
-        
         let updateNumber = update |> List.head
         if update.Length = 1 
             && updateRules |> List.map List.last |> List.contains updateNumber then true
@@ -60,18 +58,65 @@ let filterPrintableOrders (rules: int list list) (updates: int list list) =
             then pagePrintable originalUpdate updateTail updateRules
             else false
     updates
-    |> List.map (fun update -> 
-        let result = pagePrintable update update (filterRules update)
-        (update,result))
-    |> List.filter snd
-    |> List.map fst
+    |> List.map (fun update -> (update,pagePrintable update update (filterRules rules update)))
+
+let fixNonPrintableOrders (rules: int list list) (updates: int list list) =
+    let rec pagePrintable (update : int list) (updateIndex: int) (updateRules: int list list) =
+        let updateNumber = update.[updateIndex]
+        if updateIndex = update.Length - 1 
+            && updateRules |> List.map List.last |> List.contains updateNumber 
+            && updateRules |> List.map List.head |> List.contains updateNumber |> not
+        then update
+        else 
+            let updateTail = update |> List.tail
+            let updateBeforeList = update |> List.take updateIndex
+            
+            let matchingRules = 
+                updateRules
+                |> List.filter (fun f -> 
+                    f |> List.head = updateNumber && updateTail |> List.contains (f |> List.last))
+            let conflictingRules =
+                updateRules
+                |> List.filter (fun (f: int list) -> 
+                    f |> List.head = updateNumber && updateBeforeList |> List.contains (f |> List.last))
+            if conflictingRules.Length > 0
+            then 
+                pagePrintable (update |> List.removeAt updateIndex |> List.insertAt (updateIndex - 1) updateNumber) 0 updateRules 
+            else pagePrintable update (updateIndex + 1) updateRules
+    updates
+    |> List.map (fun update -> (pagePrintable update 0 (filterRules rules update)))
+
+let calculateResult (updates: int list list) =
+    updates  
     |> List.map (fun u -> u.[u.Length / 2])
     |> List.sum
 
 let (exampleRules, exampleUpdates) = parseInput "./input/day05_example.txt" 
-filterPrintableOrders exampleRules exampleUpdates
-|> printfn "Example answer 1: %A"
+let exampleResult = filterPrintableOrders exampleRules exampleUpdates
+exampleResult
+|> List.filter snd
+|> List.map fst
+|> calculateResult
+|> printfn "Example answer 1: %d"
 
 let (rules, updates) = parseInput "./input/day05.txt" 
-filterPrintableOrders rules updates
-|> printfn "Example answer 1: %A"
+let result = filterPrintableOrders rules updates
+result
+|> List.filter snd
+|> List.map fst
+|> calculateResult
+|> printfn "Answer 1: %d"
+
+exampleResult
+|> List.filter (snd >> not)
+|> List.map fst
+|> fixNonPrintableOrders exampleRules
+|> calculateResult
+|> printfn "Example answer 2: %d"
+
+result
+|> List.filter (snd >> not)
+|> List.map fst
+|> fixNonPrintableOrders rules
+|> calculateResult
+|> printfn "Answer 2: %d"
